b76cc47e85e365ee8980cbd1f2a384ee19057746
mwhitfieldwork <mwhitfieldwork@gmail.com>
2024-11-22 12:37:31 -0600

    Extract into smaller compoents use content projection

---
+++ 01-starting-project/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, input, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css'
+})
+export class DashboardItemComponent {
+@Input({required: true}) image!:{src:string, alt:string};
+ title = input.required<string>(); //same thing as above;
+ 
+}
