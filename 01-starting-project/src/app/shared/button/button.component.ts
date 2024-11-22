a85392905fec1e620c7a47b75dae8bcc9fea7c10
mwhitfieldwork <mwhitfieldwork@gmail.com>
2024-11-22 14:12:21 -0600

    Add content-projection and extend buttons

---
+++ 01-starting-project/src/app/shared/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button.appButton', //attribute selector usage, any thing that has this attribute will be replace by this content
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
