439ca5558e5d63f87916bcb635ffd818d2064b88
mwhitfieldwork <mwhitfieldwork@gmail.com>
2024-11-23 09:37:16 -0600

    intro to using host elements

--- 01-starting-project/src/app/shared/control/control.component.ts
+++ 01-starting-project/src/app/shared/control/control.component.ts
@@ -1,11 +1,16 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-control',
+  selector: 'app-control',// THE APP-CONTROL SELECTOR - THIS IS ALSO THE HOST ELEMENT - WHICH CAN BE TARGETED BY CSS
+  //BY USING THE :host SELECTOR
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None // we  need to disable the scope of the css. So 
+                                        //we need to scope the css of this component globally to account for the content projected markup
+                                        
+                                        
 })
 export class ControlComponent {
 label = input.required<string>();
