dffd6f2471e95a95b34a32fd298e047f86a647bb
mwhitfieldwork <mwhitfieldwork@gmail.com>
2024-11-24 10:12:49 -0600

    Allow for tickets to be visible in the queue -140

--- 01-starting-project/src/app/dashboard/tickets/tickets.component.ts
+++ 01-starting-project/src/app/dashboard/tickets/tickets.component.ts
@@ -1,11 +1,12 @@
 import { Component, ElementRef, HostBinding, HostListener, inject } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 import { Ticket } from './ticket.model';
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
   host : {
